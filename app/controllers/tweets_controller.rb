ac8c84896d474f0bf67a44027fc4b5113877f49b
kajiya-satsuki <shupeen.soykiki@gmail.com>
2020-11-08 14:59:16 +0900

    created new function

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,4 +2,19 @@ class TweetsController < ApplicationController
     def index
         @tweets = Tweet.all
     end
+
+    def new
+        @tweet = Tweet.new
+    end
+
+    def create
+        @tweet = Tweet.create(tweet_params)
+        redirect_to :root
+    end
+
+    private
+    def tweet_params
+        params.require(:tweet).permit(:title, :text)
+    end
+    
 end
